3081df1239b7d9ec95b5229078d31ea44a67eba9
dimitra86 <zliden01@protonmail.com>
2020-02-29 16:36:13 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    if (n > 0) {
+
+        let x = n % 100;
+        let hundred = (n - x) / 100;
+        let one = x % 10;
+        let ten = (x - one) / 10;
+        let rezult = one * 100 + ten * 10 + hundred;
+        return rezult;
+
+
+    } else if (n < 0) {
+
+        let x = Math.abs(n) % 100;
+        let hundred = (Math.abs(n) - x) / 100;
+        let one = x % 10;
+        let ten = (x - one) / 10;
+        let rezult = one * 100 + ten * 10 + hundred;
+        return rezult;
+    }
 }
